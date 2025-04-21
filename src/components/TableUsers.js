8ffb7b03a7279cbab6ca1be2bb3155a8229f2916
Huy Duc Nguyen <huy.nguyenduc@asnet.com.vn>
2025-04-21 12:31:58 +0700

    Render List Users

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Table from "react-bootstrap/Table";
+import { fetchAllUsers } from "../services/UserService";
 
 const TableUsers = () => {
-  return <div>TableUsers</div>;
+  const [listUsers, setListUsers] = useState([]);
+  const getUsers = async () => {
+    const res = await fetchAllUsers();
+    if (res && res.data && res.data.data) {
+      setListUsers(res.data.data);
+    }
+  };
+  useEffect(() => {
+    // CALL APIS
+    getUsers();
+  }, []);
+
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Email</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        {listUsers &&
+          listUsers.length > 0 &&
+          listUsers.map((item) => {
+            return (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.email}</td>
+                <td>{item.first_name}</td>
+                <td>{item.last_name}</td>
+              </tr>
+            );
+          })}
+      </tbody>
+    </Table>
+  );
 };
 
 export default TableUsers;
