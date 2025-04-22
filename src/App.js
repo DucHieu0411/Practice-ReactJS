4d0d79ae3e228c87aa0c5e382b06f15e5328de22
Huy Duc Nguyen <huy.nguyenduc@asnet.com.vn>
2025-04-22 15:31:04 +0700

    Design modal add new users

--- src/App.js
+++ src/App.js
@@ -2,14 +2,37 @@ import { Container } from "react-bootstrap";
 import "./App.scss";
 import Header from "./components/Header";
 import TableUsers from "./components/TableUsers";
+import ModalAddNew from "./components/ModalAddNew";
+import { useState } from "react";
 
 function App() {
+  const [isShowModalAddNew, setIsShowModalAddNew] = useState(false);
+  const handleClose = () => {
+    setIsShowModalAddNew(false);
+  };
   return (
     <div className="app-container">
       <Header />
       <Container>
+        <div
+          className="my-3 add-new"
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignContent: "center",
+          }}
+        >
+          <span>List Users: </span>
+          <button
+            className="btn btn-success"
+            onClick={() => setIsShowModalAddNew(true)}
+          >
+            Add new user
+          </button>
+        </div>
         <TableUsers />
       </Container>
+      <ModalAddNew show={isShowModalAddNew} handleClose={handleClose} />
     </div>
   );
 }
